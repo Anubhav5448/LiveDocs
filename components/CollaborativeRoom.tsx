95088505b16a4cc86615484f989c6049c3109fa6
Anubhav yadav <anubhav1374@gmail.com>
2024-08-16 14:03:01 +0530

    third commit

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -4,24 +4,114 @@ import { RoomProvider, ClientSideSuspense } from "@liveblocks/react/suspense";
 import Header from "./Header";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { Editor } from "./editor/Editor";
+import ActiveCollaborators from "./ActiveCollaborators";
+import React, { useEffect, useRef, useState } from "react";
+import { Input } from "./ui/input";
+import Image from "next/image";
+import { updateDocument } from "@/lib/actions/room.actions";
+import Loader from "./Loader";
 
-const CollaborativeRoom = () => {
+const CollaborativeRoom = ({
+  roomId,
+  roomMetadata,
+  users,
+  currentUserType,
+}: CollaborativeRoomProps) => {
+  
+  const [documentTitle, setDocumentTitle] = useState(roomMetadata.title);
+  const [editing, setEditing] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLDivElement>(null);
+  const updateTitleHandler = async (
+    e: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    if (e.key === "Enter") {
+      try {
+        if (documentTitle !== roomMetadata.title) {
+          const updatedDocument = await updateDocument(roomId, documentTitle);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (
+        containerRef.current &&
+        !containerRef.current.contains(e.target as Node)
+      ) {
+        setEditing(false);
+        updateDocument(roomId, documentTitle);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [roomId, documentTitle]);
+
+  useEffect(() => {
+    if (editing && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [editing]);
+
   return (
-    <RoomProvider id="my-room">
-      <ClientSideSuspense fallback={<div>Loading…</div>}>
+    <RoomProvider id={roomId}>
+      <ClientSideSuspense fallback={<Loader />}>
         <div className="collaborative-room">
           <Header>
-            <div className="flex w-fit items-center justify-center gap-2 ">
-              <p className="document-title">Share</p>
+            <div
+              ref={containerRef}
+              className="flex w-fit items-center justify-center gap-2 "
+            >
+              {editing && !loading ? (
+                <Input
+                  type=" text"
+                  value={documentTitle}
+                  ref={inputRef}
+                  placeholder="Enter Title"
+                  onChange={(e) => setDocumentTitle(e.target.value)}
+                  onKeyDown={updateTitleHandler}
+                  disabled={!editing}
+                  className="document-title-input"
+                />
+              ) : (
+                <>
+                  <p className="document-title">{documentTitle}</p>
+                </>
+              )}
+              {currentUserType === "editor" && !editing && (
+                <Image
+                  alt="edit"
+                  src="/assets/icons/edit.svg"
+                  width={24}
+                  height={24}
+                  onClick={() => setEditing(true)}
+                  className="pointer"
+                />
+              )}
+
+              {currentUserType === "editor" && !editing && (
+                <p className="view-only-tag">View Only</p>
+              )}
+              {loading && <p className="text-sm text-gray-400">Saving...</p>}
             </div>
-            <SignedOut>
-              <SignInButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
+            <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
+              <ActiveCollaborators />
+
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
           </Header>
-          <Editor />
+          <Editor roomId={roomId} currentT />
         </div>
       </ClientSideSuspense>
     </RoomProvider>
--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -1,11 +1,13 @@
-"use server"; 
+"use server";
 
 import { nanoid } from "nanoid";
 import { liveblocks } from "../liveblocks";
 import { revalidatePath } from "next/cache";
 import { parseStringify } from "../utils";
 
-export const createDocument = async ({ userId,email,
+export const createDocument = async ({
+  userId,
+  email,
 }: CreateDocumentParams) => {
   const roomId = nanoid();
 
@@ -28,5 +30,52 @@ export const createDocument = async ({ userId,email,
     return parseStringify(room);
   } catch (error) {
     console.log(`Error happpened while creating a room: ${error}`);
+  }
+};
+
+export const getDocument = async ({
+  roomId,
+  userId,
+}: {
+  roomId: string;
+  userId: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+
+    const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+
+    // if (!hasAccess) {
+    //   throw new Error("You do not have access to this Document");
+    // }
+
+    return parseStringify(room);
+  } catch (error) {
+    console.log(`Error happened while getting a room : ${error}`);
+  }
+};
+
+export const updateDocument = async (roomId: string, title: string) => {
+  try {
+    const updatedRoom = await liveblocks.updateRoom(roomId, {
+      metadata: {
+        title,
+      },
+    });
+    revalidatePath(`/document/${roomId}`);
+
+    return parseStringify(updatedRoom);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getDocuments = async (email: string) => {
+  try {
+    const rooms = await liveblocks.getRooms({ userId: email });
+
+    return parseStringify(rooms);
+  } catch (error) {
+    console.log(`Error happened while getting rooms : ${error}`);
   }
 };
